70fc3e754b79863adb2498e2647856d1431fa9bb
Khanh Hong <khanhhn@vietmap.vn>
2022-05-26 23:36:31 +0700

    create form contact

---
+++ client/src/App.js
@@ -0,0 +1,13 @@
+import "./App.css";
+import "antd/dist/antd.min.css";
+import ContactForm from "./Form";
+
+function App() {
+  return (
+    <div className="App" style={{ paddingTop: "100px" }}>
+      <ContactForm />
+    </div>
+  );
+}
+
+export default App;
---
+++ client/src/Form.jsx
@@ -0,0 +1,82 @@
+import { Button, Form, Input } from "antd";
+
+const ContactForm = () => {
+  const onFinish = values => {
+    console.log("Success:", values);
+  };
+
+  const onFinishFailed = errorInfo => {
+    console.log("Failed:", errorInfo);
+  };
+
+  return (
+    <Form
+      name="basic"
+      labelCol={{
+        span: 8,
+      }}
+      wrapperCol={{
+        span: 16,
+      }}
+      onFinish={onFinish}
+      onFinishFailed={onFinishFailed}
+      autoComplete="off"
+    >
+      <Form.Item
+        label="Username"
+        name="username"
+        rules={[
+          {
+            required: true,
+            message: "Please input your username!",
+          },
+        ]}
+      >
+        <Input />
+      </Form.Item>
+
+      <Form.Item
+        label="Phone"
+        name="phone"
+        rules={[
+          {
+            required: true,
+            message: "Please input your phone!",
+          },
+        ]}
+      >
+        <Input />
+      </Form.Item>
+
+      <Form.Item label="Title" name="title">
+        <Input />
+      </Form.Item>
+
+      <Form.Item
+        label="Question"
+        name="question"
+        rules={[
+          {
+            required: true,
+            message: "Please input your question!",
+          },
+        ]}
+      >
+        <Input />
+      </Form.Item>
+
+      <Form.Item
+        wrapperCol={{
+          offset: 8,
+          span: 16,
+        }}
+      >
+        <Button type="primary" htmlType="submit">
+          Submit
+        </Button>
+      </Form.Item>
+    </Form>
+  );
+};
+
+export default ContactForm;
